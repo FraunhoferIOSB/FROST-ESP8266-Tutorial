c18c3a4929714198522473379b0120760f8fc0e0
Jacoby <michael.jacoby@iosb.fraunhofer.de>
2020-02-28 14:10:48 +0100

    updated init script

---
+++ RaspberryPi/frost-demo.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+### BEGIN INIT INFO
+# Provides:          FROST demo
+# Required-Start:
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start & stops FROST demo docker container
+# Description:       Start & stops FROST demo docker container
+### END INIT INFO
+
+start() {
+	docker-compose -f $DIR/docker-compose.yaml up
+}
+
+stop() {
+	docker-compose -f $DIR/docker-compose.yaml down
+}
+
+case "$1" in
+  start)
+	start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  status)
+    docker-compose -f $DIR/docker-compose.yaml ps
+    ;;
+    *)
+       echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0
--- RaspberryPi/init.sh
+++ RaspberryPi/init.sh
@@ -2,24 +2,17 @@
 #################################
 ### init script ###
 #################################
-AUTO_LOGIN=0
-CREATE_USER=0
-SECURE_USER=0
-SET_KEYBOARD_LAYOUT=0
 WORK_DIR="./FROST-ESP8266-Tutorial/RaspberryPi"
 GIT_URL="https://github.com/FraunhoferIOSB/FROST-ESP8266-Tutorial.git"
 DEMO_USER="demo"
 DEMO_USER_PWD="frostdemo"
 WIFI_SSID="frost-network"
 WIFI_PWD="frostWEB"
-PI_USER_PWD="sweetPIE"
-KEYBOARD_LAYOUT="de"
-INTERFACE_COMMENT"#DEMO_INTERFACE"
 
-CURRENT_DIR=`dirname $(realpath -s $0)`
-SCRIPT_FILE=`basename "$0"`
-
-### parse CLI arguments
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run a root!"
+   exit 1
+fi
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -27,18 +20,6 @@ do
 key="$1"
 
 case $key in
-    --create-user)
-    CREATE_USER=1
-    shift
-    ;;
-	-s|--secure)
-    SECURE_USER=1
-    shift
-    ;;
-	--auto-login)
-    AUTO_LOGIN=1
-    shift
-    ;;
     -u|--user)
     DEMO_USER="$2"
     shift
@@ -59,17 +40,6 @@ case $key in
     shift
     shift
     ;;
-	--new-pwd)
-    PI_USER_PWD="$2"
-    shift
-    shift
-    ;;
-	--keyboard-layout)
-    SET_KEYBOARD_LAYOUT=1
-	KEYBOARD_LAYOUT="$2"
-    shift
-    shift
-    ;;
     --default)
     DEFAULT=YES
     shift
@@ -82,45 +52,16 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-
-if [ $SET_KEYBOARD_LAYOUT -eq 1 ]
+if [ -z "$(getent passwd $DEMO_USER)" ]
 then
-	echo 'setting keyboard layout to $KEYBOARD_LAYOUT'
-	sudo sed -i "s/^#XKBLAYOUT=.*/XKBLAYOUT=\"$KEYBOARD_LAYOUT\"/" /etc/default/keyboard
+	echo "creating user: $DEMO_USER with password $DEMO_USER_PWD"
+	sudo adduser --quiet --disabled-password --gecos "demo user" --ingroup sudo $DEMO_USER
 fi
-
-if [ $SECURE_USER -eq 1 ]
-then
-	echo "changing password of user pi to $PI_USER_PWD"
-	echo "pi:$PI_USER_PWD" | sudo chpasswd
-fi
-
-if [ $CREATE_USER -eq 1 ]
-then
-	if [ -z "$(getent passwd $DEMO_USER)" ]
-	then
-		echo "creating user: $DEMO_USER with password $DEMO_USER_PWD"
-		sudo adduser --quiet --disabled-password --gecos "demo user" --ingroup sudo $DEMO_USER
-	fi
-	echo "updating password for user: $DEMO_USER to $DEMO_USER_PWD"
-	echo "$DEMO_USER:$DEMO_USER_PWD" | sudo chpasswd
-else
-	DEMO_USER=$USER
-fi
-
-if [ $AUTO_LOGIN -eq 1 ]
-then
-	echo "set auto-login to demo user"
-	mkdir -pv /etc/systemd/system/getty@tty1.service.d
-	cat >/etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOL
-[Service]
-ExecStart=-/sbin/agetty --autologin $DEMO_USER --noclear I 38400 linux
-EOL
-fi
+echo "$DEMO_USER:$DEMO_USER_PWD" | sudo chpasswd
 
 echo "installing docker ..."
 sudo curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $DEMO_USER
 sudo apt-get install -y python python-pip libffi-dev libssl-dev
 sudo apt-get remove -y python-configparser
 sudo pip install docker-compose
@@ -137,8 +78,15 @@ else
     cd $GIT_LOCAL
     git pull "$GIT_URL"
 fi
+WORK_DIR=$GIT_LOCAL/RaspberryPi
 
 sudo chown -R $DEMO_USER $GIT_LOCAL
+
+docker-compose -f $WORK_DIR/docker-compose.yaml pull
+sudo cp -rf $WORK_DIR/frost-demo.sh /etc/init.d/frost-demo
+sudo sed -i "s/\$DIR/$(echo "$WORK_DIR" | sed "s;/;\\\/;g")/" /etc/init.d/frost-demo
+sudo update-rc.d -f frost-demo remove
+sudo update-rc.d frost-demo defaults
 
 echo "setting up wifi hotspot ..."
 sudo apt-get update && apt-get -y install hostapd dnsmasq
@@ -148,15 +96,15 @@ sudo systemctl stop hostapd
 echo "denyinterfaces wlan0" | sudo tee -a /etc/dhcpcd.conf > /dev/null
 # configure static IP for WiFi
 if ! grep -q "#DEMO_INTERFACE" /etc/network/interfaces; then
-  sudo cat $GIT_LOCAL/RaspberryPi/new_interface >> /etc/network/interfaces
+  sudo cat $WORK_DIR/new_interface >> /etc/network/interfaces
 fi
 # deploy hostapd config
-sudo cp -rf $GIT_LOCAL/RaspberryPi/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp -rf $WORK_DIR/hostapd.conf /etc/hostapd/hostapd.conf
 sudo sed -i "s/^ssid=.*/ssid=$WIFI_SSID/" /etc/hostapd/hostapd.conf
 sudo sed -i "s/^wpa_passphrase=.*/wpa_passphrase=$WIFI_PWD/" /etc/hostapd/hostapd.conf
 echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" | sudo tee -a /etc/default/hostapd > /dev/null
 # configure dnsmasq
-sudo cp -rf $GIT_LOCAL/RaspberryPi/dnsmasq.conf /etc/dnsmasq.conf
+sudo cp -rf $WORK_DIR/dnsmasq.conf /etc/dnsmasq.conf
 sudo systemctl start dnsmasq
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
